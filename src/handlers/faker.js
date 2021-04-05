60318237055ce5160e153b839197aaf1e8bed6c0
liberovitae <admin@liberovitae.com>
2021-04-06 00:44:14 +0100

    Improve fake data generator

--- src/handlers/faker.js
+++ src/handlers/faker.js
@@ -5,6 +5,57 @@ import eventTypes from '../constants/eventTypes';
 import cities from 'all-the-cities';
 import faker from 'faker';
 import generateSlug from './generateSlug';
+import ora from 'ora';
+
+const dataTypes = [
+  {
+    title: () => faker.name.jobTitle(),
+    image: () => faker.image.business(200, 200, true),
+    hasComments: false,
+    hasDates: false,
+    hasLocation: true,
+    type: 'job',
+    requiresParent: true,
+    parentType: 'company',
+    types: jobTypes,
+  },
+  {
+    title: () => `${faker.company.companyName()}`,
+    image: () => faker.image.business(200, 200, true),
+    hasComments: false,
+    hasDates: false,
+    hasLocation: true,
+    type: 'company',
+    requiresParent: false,
+    types: venueTypes,
+  },
+  {
+    title: () =>
+      `${faker.address.city()} ${faker.commerce.product()}`,
+    image: () => faker.image.nightlife(200, 200, true),
+    hasComments: true,
+    hasDates: false,
+    hasLocation: true,
+    type: 'venue',
+    requiresParent: false,
+    types: venueTypes,
+  },
+  {
+    title: () =>
+      `${faker.address.city()} ${faker.commerce.department()} ${
+        Math.random() < 0.5 && faker.date.weekday()
+      }`,
+    image: () => faker.image.nightlife(200, 200, true),
+
+    hasComments: true,
+    hasDates: true,
+    hasLocation: true,
+    type: 'event',
+    requiresParent: true,
+    parentType: 'venue',
+    types: eventTypes,
+  },
+];
 
 const createFakeUser = async () => {
   try {
@@ -12,9 +63,9 @@ const createFakeUser = async () => {
       username: faker.internet.userName(),
       email: faker.internet.email(),
       password: faker.internet.password(
-        8,
+        9,
         false,
-        /^[a-zA-Z0-9_.-]*$/,
+        /^[a-zA-Z0-9!@#$&()\\-`.+,/\"]*$/,
       ),
       verified: true, // Verify our fake user otherwise the autodeletion runs
     };
@@ -31,44 +82,22 @@ const createFakeUser = async () => {
   }
 };
 
-const createFakeCompany = async (user) => {
+const createFakePost = async ({ parent, dataType, userId }) => {
   try {
-    const fakeCompany = {
-      title: faker.company.companyName(),
-      image: faker.image.business(200, 200, true),
-      website: faker.internet.url(),
-      tagline: faker.hacker.phrase(),
-      userId: user._id,
-    };
+    const { title, type, hasDates, hasComments, image } = dataType;
 
-    const company = await models.Company.create(fakeCompany);
-
-    await models.User.findByIdAndUpdate(user._id, {
-      company: company._id,
-    });
-
-    if (company) {
-      return company;
-    } else {
-      throw new Error();
-    }
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-const createFakeJob = async (user, company) => {
-  try {
-    const fakeCity =
-      cities[Math.floor(Math.random() * cities.length)];
-
-    let location;
-
+    // Generate fake tags
     let tags = [];
 
     for (let i = 0; i < Math.floor(Math.random() * 10 + 1); i++) {
       tags.push(faker.random.word());
     }
+
+    // Generate fake location
+    const fakeCity =
+      cities[Math.floor(Math.random() * cities.length)];
+
+    let location;
 
     if (fakeCity.country === 'US') {
       location = {
@@ -84,83 +113,22 @@ const createFakeJob = async (user, company) => {
       };
     }
 
-    const fakeJob = {
-      title: faker.name.jobTitle(),
-      location: location,
-      description: `<h1>${faker.lorem.words(6)}</h1>
-      <p>
-      ${faker.lorem.paragraphs(5)}
-      </p>
-      <ul>
-      <li>${faker.lorem.words(5)}</li>
-      <li>${faker.lorem.words(3)}</li>
-      <li>${faker.lorem.words(6)}</li>
-      <li>${faker.lorem.words(8)}</li>
-      </ul>
-      <p>
-      ${faker.lorem.paragraphs(8)}
-      </p>
-      <p>Contact: ${faker.internet.email()} or ${faker.phone.phoneNumber()}
-      `,
-      types: jobTypes[Math.floor(Math.random() * jobTypes.length)],
-      url:
-        Math.random() < 0.5
-          ? faker.internet.url()
-          : faker.internet.email(),
-      tags: tags,
-    };
+    let startDate;
+    let endDate;
 
-    const job = await models.Job.create({
-      slug: generateSlug(fakeJob.title),
-      parent: company._id,
-      parentName: company.title,
-      status: 'published',
-      publishedAt: Date.now(),
-      ...fakeJob,
-    });
+    // Generate fake (start) date
+    hasDates
+      ? ((startDate = faker.date.recent()),
+        (endDate = faker.date.soon(3, startDate)))
+      : null;
 
-    if (job) {
-      return job;
-    } else {
-      return false;
-    }
-  } catch (err) {
-    console.log(err);
-    return false;
-  }
-};
+    const postTitle = title();
 
-const createFakeVenue = async (user) => {
-  try {
-    const fakeCity =
-      cities[Math.floor(Math.random() * cities.length)];
-
-    let location;
-
-    let tags = [];
-
-    for (let i = 0; i < Math.floor(Math.random() * 10 + 1); i++) {
-      tags.push(faker.random.word());
-    }
-
-    if (fakeCity.country === 'US') {
-      location = {
-        name: `${fakeCity.name}, ${fakeCity.adminCode}, ${fakeCity.country}`,
-        lat: fakeCity.lat,
-        lon: fakeCity.lon,
-      };
-    } else {
-      location = {
-        name: `${fakeCity.name}, ${fakeCity.country}`,
-        lat: fakeCity.lat,
-        lon: fakeCity.lon,
-      };
-    }
-
-    const fakeVenue = {
-      title: faker.commerce.productName(),
-      location: location,
-      description: `<h1>${faker.lorem.words(6)}</h1>
+    const fakePost = {
+      title: postTitle,
+      type,
+      location,
+      text: `<h1>${faker.lorem.words(6)}</h1>
       <p>
       ${faker.lorem.paragraphs(5)}
       </p>
@@ -176,27 +144,36 @@ const createFakeVenue = async (user) => {
       <p>Contact: ${faker.internet.email()} or ${faker.phone.phoneNumber()}
       `,
       types:
-        venueTypes[Math.floor(Math.random() * venueTypes.length)],
+        dataType.types[
+          Math.floor(Math.random() * dataType.types.length)
+        ],
       url:
         Math.random() < 0.5
           ? faker.internet.url()
           : faker.internet.email(),
       tags: tags,
+      userId: userId,
+      commentsEnabled:
+        hasComments && Math.random() < 0.5 ? true : false,
+      slug: generateSlug(postTitle),
+      status: 'published',
+      dates: hasDates
+        ? {
+            start: startDate,
+            end: endDate,
+          }
+        : null,
+      parent: parent,
+      image: image(),
+      publishedAt: Date.now(),
     };
 
-    const venue = await models.Venue.create({
-      userId: user.id,
-      slug: generateSlug(fakeVenue.title),
-      status: 'published',
-      image: faker.image.city(200, 200, true),
-      publishedAt: Date.now(),
-      ...fakeVenue,
-    });
+    const post = await models.Post.create(fakePost);
 
-    if (venue) {
-      return venue;
+    if (post) {
+      return post;
     } else {
-      return false;
+      return;
     }
   } catch (err) {
     console.log(err);
@@ -204,101 +181,77 @@ const createFakeVenue = async (user) => {
   }
 };
 
-const createFakeEvent = async (venue) => {
-  try {
-    const fakeCity =
-      cities[Math.floor(Math.random() * cities.length)];
-
-    let location;
-
-    let tags = [];
-
-    for (let i = 0; i < Math.floor(Math.random() * 10 + 1); i++) {
-      tags.push(faker.random.word());
-    }
-
-    if (fakeCity.country === 'US') {
-      location = {
-        name: `${fakeCity.name}, ${fakeCity.adminCode}, ${fakeCity.country}`,
-        lat: fakeCity.lat,
-        lon: fakeCity.lon,
-      };
-    } else {
-      location = {
-        name: `${fakeCity.name}, ${fakeCity.country}`,
-        lat: fakeCity.lat,
-        lon: fakeCity.lon,
-      };
-    }
-
-    const startDate = faker.date.recent();
-
-    const fakeEvent = {
-      title: `${faker.music.genre()} ${faker.address.city()} ${faker.commerce.product()} ${faker.date.weekday()}`,
-      location: location,
-      description: `<h1>${faker.lorem.words(6)}</h1>
-      <p>
-      ${faker.lorem.paragraphs(5)}
-      </p>
-      <ul>
-      <li>${faker.lorem.words(5)}</li>
-      <li>${faker.lorem.words(3)}</li>
-      <li>${faker.lorem.words(6)}</li>
-      <li>${faker.lorem.words(8)}</li>
-      </ul>
-      <p>
-      ${faker.lorem.paragraphs(8)}
-      </p>
-      <p>Contact: ${faker.internet.email()} or ${faker.phone.phoneNumber()}
-      `,
-      types:
-        eventTypes[Math.floor(Math.random() * eventTypes.length)],
-      url:
-        Math.random() < 0.5
-          ? faker.internet.url()
-          : faker.internet.email(),
-      tags: tags,
-    };
-
-    const event = await models.Event.create({
-      slug: generateSlug(fakeEvent.title),
-      status: 'published',
-      dates: {
-        start: startDate,
-        end: faker.date.soon(3, startDate),
-      },
-      parent: venue,
-      image: faker.image.nightlife(200, 200, true),
-      publishedAt: Date.now(),
-      ...fakeEvent,
-    });
-
-    if (event) {
-      return event;
-    } else {
-      return false;
-    }
-  } catch (err) {
-    console.log(err);
-    return false;
+const dataTypePicker = async (parent) => {
+  if (parent) {
+    const parents = dataTypes.filter((type) => !type.requiresParent);
+    return parents[Math.floor(Math.random() * parents.length)];
   }
+
+  const children = dataTypes.filter((type) => type.requiresParent);
+  return children[Math.floor(Math.random() * children.length)];
 };
 
 const createCompleteFake = async () => {
+  const LIMIT = 250;
   try {
-    for (let step = 0; step < 250; step++) {
-      const user = await createFakeUser();
+    const spinner = ora('Creating fake data').start();
 
-      const company = await createFakeCompany(user);
+    // Create users
+    // for (let step = 0; step < LIMIT; step++) {
+    // spinner.text = 'Creating fake users'
 
-      const job = await createFakeJob(user, company);
+    //   const user = await createFakeUser();
+    // }
 
-      const venue = await createFakeVenue(user);
+    // Create parent posts
+    for (let step = 0; step < LIMIT; step++) {
+      spinner.text = 'Creating fake parent posts';
 
-      const event = await createFakeEvent(venue);
+      const dataType = await dataTypePicker(true);
 
-      console.log('Created fake data', step);
+      const randomUsers = await models.User.aggregate([
+        { $sample: { size: 1 } },
+      ]);
+
+      const parent = await createFakePost({
+        dataType,
+        userId: randomUsers[0],
+      });
+
+      await models.User.findByIdAndUpdate(randomUsers[0]._id, {
+        $addToSet: { posts: parent },
+      });
     }
+
+    // Create child posts
+    for (let step = 0; step < LIMIT; step++) {
+      spinner.text = 'Creating fake child posts';
+
+      const dataType = await dataTypePicker();
+
+      const randomPosts = await models.Post.aggregate([
+        { $match: { type: dataType.parentType } },
+        { $sample: { size: 1 } },
+      ]);
+
+      const user = await models.User.findOne(randomPosts[0].userId);
+
+      const child = await createFakePost({
+        parent: randomPosts[0],
+        dataType,
+        userId: user,
+      });
+
+      await models.Post.findByIdAndUpdate(randomPosts[0]._id, {
+        $addToSet: { children: child },
+      });
+
+      user.posts.push(child);
+      user.save();
+    }
+
+    spinner.stop();
+    // Create comment threads
   } catch (err) {
     console.log(err);
     return false;
