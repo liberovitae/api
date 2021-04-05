6a7c524168a32989dcb8ed6e0d1e7fb237abb469
liberovitae <admin@liberovitae.com>
2021-04-06 00:43:46 +0100

    Update initial search filter

--- src/constants/initialSearchFilter.js
+++ src/constants/initialSearchFilter.js
@@ -5,3 +5,16 @@ const INITIAL_STATE = {
 };
 
 export default INITIAL_STATE;
+export default {
+  keywords: '',
+  location: {
+    name: '',
+    lat: 0,
+    lon: 0,
+  },
+  dates: {
+    start: null,
+    end: null,
+  },
+  types: [],
+};
